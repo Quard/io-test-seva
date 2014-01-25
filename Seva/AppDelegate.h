7fb43220cc7bb6dd8c97a0373a740e5783f12df0
Vadym Zakovinko <vadym@zakovinko.com>
2014-01-25 12:00:25 +0200

    Initial Commit

---
+++ Seva/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Seva
+//
+//  Created by Vadym Zakovinko on 1/25/14.
+//  Copyright (c) 2014 Vadym Zakovinko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ SevaTests/SevaTests.m
@@ -0,0 +1,34 @@
+//
+//  SevaTests.m
+//  SevaTests
+//
+//  Created by Vadym Zakovinko on 1/25/14.
+//  Copyright (c) 2014 Vadym Zakovinko. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SevaTests : XCTestCase
+
+@end
+
+@implementation SevaTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
